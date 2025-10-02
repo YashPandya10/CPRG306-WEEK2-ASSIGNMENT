87fbb6bd20e370ca1edd40c8efd8e92100bbc295
YashPandya10 <123474152+YashPandya10@users.noreply.github.com>
2025-10-02 11:39:05 -0600

    changes

--- app/week-3/item-list.js
+++ app/week-3/item-list.js
@@ -1,17 +1,20 @@
 import Item from "./item";
-import { items } from "./items";
 
 export default function ItemList() {
   return (
-    <ul className="space-y-3 mt-4">
-      {items.map((item, index) => (
-        <Item
-          key={index}
-          name={item.name}
-          quantity={item.quantity}
-          category={item.category}
-        />
-      ))}
+     <ul className="space-y-3 mt-4">
+      <Item name="milk, 4 L 🥛" quantity={1} category="dairy" />
+      <Item name="bread 🍞" quantity={2} category="bakery" />
+      <Item name="eggs, dozen 🥚" quantity={2} category="dairy" />
+      <Item name="bananas 🍌" quantity={6} category="produce" />
+      <Item name="broccoli 🥦" quantity={3} category="produce" />
+      <Item name="chicken breasts, 1 kg 🍗" quantity={1} category="meat" />
+      <Item name="pasta sauce 🍝" quantity={3} category="canned goods" />
+      <Item name="spaghetti, 454 g 🍝" quantity={2} category="dry goods" />
+      <Item name="toilet paper, 12 pack 🧻" quantity={1} category="household" />
+      <Item name="paper towels, 6 pack" quantity={1} category="household" />
+      <Item name="dish soap 🍽️" quantity={1} category="household" />
+      <Item name="hand soap 🧼" quantity={4} category="household" />
     </ul>
   );
 }
